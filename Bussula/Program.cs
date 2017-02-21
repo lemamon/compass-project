640f959d990be31f65365c7447a095b01eb7d943
lemamon <demmarinho@gmail.com>
2017-02-21 14:35:45 -0400

    create compass project for a challege with c#

---
+++ Bussula/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussula
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            string coordinates = Console.ReadLine();
+            int position = 0;
+            string direction = "NLSO";
+            string accumulator = "";
+
+           
+            foreach(char c in coordinates)
+            {
+                if(c == 'D' || c == 'd')
+                {
+                    position = position >= 3 ? 0 : position + 1;
+                    accumulator += direction[position];
+                   
+                }else if(c == 'E' || c == 'e')
+                {
+                    position = position <= 0 ? 3 : position - 1;
+                    accumulator += direction[position];
+                }
+            }
+
+            Console.WriteLine(accumulator);
+            Console.ReadLine();
+
+        }
+    }
+}
